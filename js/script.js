5c8ec013d6caf24e115fd200676fd8819319553d
Eliabe Gai <eliabegai@gmail.com>
2024-11-13 14:02:56 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function copiarEmail() {
+    const email = "eliabegai@gmail.com";
+    
+    const inputTemporario = document.createElement("input");
+    inputTemporario.value = email;
+    document.body.appendChild(inputTemporario);
+    
+    inputTemporario.select();
+    document.execCommand("copy");
+    
+    document.body.removeChild(inputTemporario);
+    
+    // Exibe a notificação
+    const notificacao = document.getElementById("notificacao");
+    notificacao.classList.add("visivel");
+
+    // Oculta a notificação após 2 segundos
+    setTimeout(() => {
+        notificacao.classList.remove("visivel");
+    }, 2000);
+}
